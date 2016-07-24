702226eee21fd1c07ec7e526ef26a75dc0810ae9
kiloreux <kiloreux@gmail.com>
2016-07-24 08:28:20 +0100

    working on the build always

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get install git
 git clone https://github.com/json-c/json-c.git json-c
 cd json-c
+bash autogen.sh
 ./configure && make && sudo make install
 cd ..
 pkg-config --cflags --libs json-c
